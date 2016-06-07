0193cb76dddbf9d29a46bab53c158651d74a0a8a
Ben Sprunger <benj.sprunger@gmail.com>
2016-06-07 00:07:20 +0000

    Added basic structure to databaseController.rb file

--- databaseController.rb
+++ databaseController.rb
@@ -1,6 +1,24 @@
+#!/usr/bin/ruby
+
+require 'mysql'
+
 class databaseController
 
 	def initialize
 	end
+	
+	def addItem
+	end
 
+	def deleteItem
+	end
+
+	def deleteAll
+	end
+
+	def displayItem
+	end
+
+	def displayAll
+	end
 end
